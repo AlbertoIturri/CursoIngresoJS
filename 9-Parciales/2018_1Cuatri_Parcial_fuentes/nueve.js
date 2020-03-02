651cc85dfc855c9e4a8024a46605e61a993fd610
AlbertoIturri <60608403+AlbertoIturri@users.noreply.github.com>
2020-03-02 12:26:30 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,45 @@
 function mostrar()
-{
+{ var marca;
+  var peso;
+  var temperatura;
+  var respuesta;
+  var contadorTemPares= 0;
+  var pesoMaximoNoCongelado;
+  var marcaPesada;
+  var flag= 0;
+  var contadorProductosCongelados= 0;
 
+  
+  
+
+  // --------------------  Repito mientras el usuario quiera -----------------------
+  do { 
+      // ---------------------- Pido datos --------------------------
+      marca = prompt("Ingrese una marca");
+          
+        peso = parseInt(prompt("Ingrese el peso entre 1 y 100 "));
+        while (peso < 1 || peso > 100 || isNaN(peso) ){
+          peso = parseInt(prompt("Peso invalido. Ingrese el peso entre 1 y 100"));
+        } temperatura = parseInt(prompt("Ingrese una temperatura entre -30 y 30 "));
+           while(temperatura < -30 || temperatura > 30 || isNaN(temperatura) ){
+               temperatura = parseInt(prompt("Temperatura invalida. Ingrese una temperatura entre -30 y 30"));
+           } // ----------------------- Pregunto si quiere continuar ---------------------------
+            respuesta = prompt("Quiere seguir ingresando?");
+            // ----------------------- Paridad -------------------------------------------
+            if (temperatura % 2 == 0 ){
+                contadorTemPares++;
+            } // ----------------------- Marca del producto mas pesado no congelado  ------------------------
+             if ((flag == 0 || peso > pesoMaximoNoCongelado )&& temperatura > 0 ){
+                 pesoMaximoNoCongelado = peso;
+                 marcaPesada = marca;
+                 flag = 1;
+             } // --------------------- Productos Congelados -------------------------
+              if (temperatura < 0 ){
+                  contadorProductosCongelados++;
+              }
+             
+             
+
+  } while (respuesta == 's' ); 
+ alert =(contadorProductosCongelados);
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,57 @@
 function mostrar()
-{
+{ var nombre;
+  var edad;
+  var sexo;
+  var nota;
+  var respuesta;
+  var contadorVaronesAprobados=0;
+  var contadorMenores= 0;
+  var contadorAdolescentes= 0;
+  var contadorMayores= 0;
+  var contadorNotasF=0;
+  var contadorNotasM=0;
+  var acumuladorMenores= 0;
+  var acumuladorAdolescentes= 0;
+  var acumuladorMayores= 0;
+  var acumuladorF= 0;
+  var acumuladorM= 0;
+  var promedioMenores= 0;
+  var promedioAdolescentes= 0;
+  var promedioMayores= 0;
+  var promedioF= 0;
+  var promedioM= 0;
+   // ----------------------- Repito hasta que el usuario quiera ------------------------
+   // ------------------------ Pido datos ---------------------------------
+    do{
+        nombre = prompt("Ingrese un nombre");
+         while(!(isNaN(nombre))){
+             nombre = prompt("No es un nombre. Ingrese un nombre");
+         } edad = parseInt(prompt("Ingrese una edad"));
+          while(isNaN(edad) || edad <= 0 ){
+              edad = parseInt(prompt("Edad invalida. Ingrese una edad"));
+          } sexo = prompt("Ingrese sexo (f o m ) ");
+           while(sexo != 'f' && sexo != 'm'){
+               sexo= prompt("Sexo invalido. Ingrese sexo (f o m) ");
+           } nota = parseInt(prompt("Ingrese una nota "));
+            while(isNaN(nota)){
+                nota = parseInt(prompt("Nota Invalida. Ingrese una nota"));
+            } if (sexo == 'm' && nota >= 4){
+                contadorVaronesAprobados++;
+
+            } if (edad < 13 ){
+                acumuladorMenores= acumuladorMenores + nota;
+                contadorMenores++
+            } else if (edad >= 13 && edad <= 17 ){
+                acumuladorAdolescentes = acumuladorAdolescentes + nota;
+                contadorAdolescentes++;
+            } else {
+                acumuladorMayores = acumuladorMayores + nota;
+                contadorMayores++;
+            }
+            respuesta = prompt("Quiere continuar?");
+
+    } while(respuesta == 's' );
+    // ---------------------- Promedios------------------------
+     
 
 }
